921f8fa2641ee5e6aae5e8094871af21945e965f
macdarat <makubyz@gmail.com>
2016-11-28 16:55:12 +0000

    Updated routing table
    fixed syntax and added some more methods

--- src/RoutingTable.java
+++ src/RoutingTable.java
@@ -1,40 +1,62 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
+/**
+ * Class describes the functionality of a Routing Table.
+ * @version 0.2 pre-Alpha
+ */
 public class RoutingTable {
-	import java.io.IOException;
-	import java.io.ObjectInputStream;
-
+	public static final int DEFAULT_COLUMNS = 3;
+	
+	private String[][] table;
+	private int length;
 	/**
-	 * Class describes the functionality of a Routing Table.
-	 * default implementation is a table with 2 columns, and variable rows
-	 * @version 0.2 pre-Alpha
+	 * Creates a new, empty routing table with the specified length
+	 * @param length
 	 */
-	public class RoutingTable {
-		public static final int DEFAULT_COLUMNS = 3;
+	public RoutingTable(int length){
+		this.length = length;
+		table = new String[length][DEFAULT_COLUMNS];
+	}
 		
-		private String[][] table;
-		private int length;
-		/**
-		 * Creates a new, empty routing table with the specified length
-		 * @param length
-		 */
-		public RoutingTable(int length){
-			this.length = length;
-			table = new String[DEFAULT_COLUMNS][length];
+	/**
+	 * Recreates an existing routing table from an ObjectInputStream
+	 * @param in -the ObjectInputStream containing the table
+	 * @throws IOException 
+	 */
+	public RoutingTable(ObjectInputStream in) throws IOException{
+		this.length = in.readInt();
+		try {
+			this.table = (String[][]) in.readObject();
+		} 
+		catch (ClassNotFoundException e) {
+			System.err.println("Table not recognised from stream");
+			e.printStackTrace();
 		}
+	}
+	
+	
+	//TODO
+	public void dijkstraAlgorithm(){
 		
-		/**
-		 * Recreates an existing routing table from an ObjectInputStream
-		 * @param in -the ObjectInputStream containing the table
-		 * @throws IOException 
-		 */
-		public RoutingTable(ObjectInputStream in) throws IOException{
-			this.length = in.readInt();
-			try {
-				this.table = (String[][]) in.readObject();
-			} 
-			catch (ClassNotFoundException e) {
-				System.err.println("Table not recognised from stream");
-				e.printStackTrace();
+	}
+	
+	public String[][] getTable(){
+		return this.table;
+	}
+	public int getLength(){
+		return this.length;
+	}
+	
+	public String toString(){
+		String result = "[";
+		
+		for(int i=0; i<this.length; i++){
+			if(table[i][0] != null){
+				result = result.concat(table[i][0] +":"+ table[i][1] +":"+ table[i][2] + " ");
 			}
 		}
+		result = result.concat("]");
+		return result;
+	}
 }
